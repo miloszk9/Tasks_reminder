048c05bcef2c5747c6874ca5b2420ef4e9337db3
miloszk9 <miloszk1990@gmail.com>
2020-10-02 14:16:19 +0200

    Added templates, added model 'Task', made the date changable (home.html)
    Added template idex.html - core, home.html - test page with changeble
    date. Added one model - "Task", created two test records.

--- main/models.py
+++ main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Task(models.Model):
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
+    name = models.CharField(max_length = 100)
+    create_date = models.DateTimeField(auto_now_add = True)
+    date = models.DateTimeField(default = timezone.now)
+    state = False
+
+    def __str__(self):
+        return self.name
+    
+    def state_change(self):
+        if state == True:
+            state = False
+        else:
+            state = True
--- main/views.py
+++ main/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
-
-from django.http import HttpResponse # for test
+from .models import Task
+from django.utils import timezone
+from django.contrib.auth.models import User
+from datetime import datetime
+from datetime import timedelta
 
 def home(request):
-    return HttpResponse('<h1>Test</h1>')
+    date_change = 0
+    if request.POST.get('next'):
+        print(request.POST.get('next') )
+        date_change = int(request.POST.get('next')) + 1
+    elif request.POST.get('previous'):
+        date_change = int(request.POST.get('previous')) - 1
+    date_get = datetime.now() + timedelta(days = date_change)
+
+    data = {   
+        'date': date_get,
+        'date_change': date_change,
+        'db_data': Task.objects.all(),
+        'title': 'Tasks reminder'
+    }
+    return render(request, 'main/home.html', data)
