8876a51f144189c19f9f244507b0680a23496151
Mylosz <miloszk1990@gmail.com>
2023-05-27 00:44:54 +0200

    Set Debug to false, add Domain variable

--- Tasks_reminder/settings.py
+++ Tasks_reminder/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = ['*']
+DEBUG = False
+ALLOWED_HOSTS = [os.getenv('DOMAIN')]
 
 
 # Application definition
